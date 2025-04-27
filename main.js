52d3002b5b2f71d75591f3ba7479e38339c25499
SoaresDev03 <alexandre.doliveira@sptech.school>
2025-04-27 17:05:44 -0300

    Atualização da pagina de login e cadastro, incluindo customização

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-function calc() { }
+// function calc() { }
+
+function togglePassword() {
+    const passwordInput = document.getElementById('inputSenha');
+    const password2Input = document.getElementById('inputSenha2');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+
+    } else {
+        passwordInput.type = 'password';
+
+    }
+
+    if (password2Input.type === 'password') {
+        password2Input.type = 'text';
+
+    } else {
+        password2Input.type = 'password';
+    }
+}
+
+function Registrar(){
+
+    alert('Usuario cadastrado com sucesso!')
+
+}
