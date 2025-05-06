a98ca6c62ca3988db1ebbba6157cb3bc0e3f162c
SoaresDev03 <alexandre.doliveira@sptech.school>
2025-05-06 14:21:35 -0300

    teste de API

--- Site/Js/main.js
+++ Site/Js/main.js
@@ -20,8 +20,67 @@ function togglePassword() {
     }
 }
 
-function Registrar(){
+function registrar() {
 
-    alert('Usuario cadastrado com sucesso!')
+    var usuario = document.getElementById('inputUsername').value;
+    var email = document.getElementById('inputEmail').value;
+    var senha = document.getElementById('inputSenha').value;
+    var senha2 = document.getElementById('inputSenha2').value
+
+    var usuarioValido = true;
+    var emailValido = true;
+    var senhaValida = true;
+    var senha2Valida = true;
+
+    if (email.indexOf('@') === -1) {
+        usuarioValido = false;
+        alert("Usuário deve conter um '@' no e-mail.");
+    }
+
+    if (senha.length < 8) {
+        senhaValida = false;
+        alert("Senha deve ter no mínimo 8 caracteres.");
+    } else {
+        var temLetraMinuscula = false;
+        var temLetraMaiuscula = false;
+        var temCaractereEspecial = false;
+        var caracteresEspeciais = "!@#$%^&*()-_=+[]{};:,.<>?/|\\";
+
+        for (var indiceSenha = 0; indiceSenha < senha.length; indiceSenha++) {
+            if (senha[indiceSenha] >= 'a' && senha[indiceSenha] <= 'z') {
+                temLetraMinuscula = true;
+            }
+            if (senha[indiceSenha] >= 'A' && senha[indiceSenha] <= 'Z') {
+                temLetraMaiuscula = true;
+            }
+            for (var indiceCaractereEspecial = 0; indiceCaractereEspecial < caracteresEspeciais.length; indiceCaractereEspecial++) {
+                if (senha[indiceSenha] === caracteresEspeciais[indiceCaractereEspecial]) {
+                    temCaractereEspecial = true;
+                }
+            }
+        }
+
+        if (!temLetraMinuscula || !temLetraMaiuscula || !temCaractereEspecial) {
+            senhaValida = false;
+            alert(` Senha deve ter pelo menos uma letra minúscula. \n
+                Senha deve ter pelo menos uma letra maiúscula. \n
+                Senha deve ter pelo menos um caractere especial.`);
+        }
+    }
+
+    if (senha2 != senha) {
+        alert("As senhas não coincidem!")
+    }
+
+    if (emailValido && senhaValida && senha2Valida) {
+        alert("Cadastro realizado com sucesso.");
+        window.location = 'login.html';
+    }
+
+}
+
+function entrar() {
+
+    window.location = 'dashboard.html';
 
 }
